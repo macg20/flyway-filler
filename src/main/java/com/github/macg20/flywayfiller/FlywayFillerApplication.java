53d21433daabfc95aa65e8079e7b2e4ee6bfec5e
Maciej.Gryczka <maciej.gryczka@asseco.pl>
2022-07-17 14:31:21 +0200

    initial commit

---
+++ src/main/java/com/github/macg20/flywayfiller/FlywayFillerApplication.java
@@ -0,0 +1,13 @@
+package com.github.macg20.flywayfiller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayFillerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayFillerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/macg20/flywayfiller/FlywayFillerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.macg20.flywayfiller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayFillerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
